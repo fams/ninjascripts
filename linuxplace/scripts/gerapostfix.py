7d307baa380131a2b9a72839a7ea2f2a87d5fea7
plucio <plucio@13f11263-bcd0-4e9e-95d0-45e2dbd4ab20>
2005-10-21 12:52:34 +0000

    Versao inicial do arquivo que gera as configuracoes do postfix a  partir do arquivo de  stanza. Esta versao ja gera o main.cf corretamente com suporte facultativo  a relay autenticado (smtp_sasl) e inclui suporte padrao a LDAP

---
+++ linuxplace/scripts/gerapostfix.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+import socket
+
+# Pega o FQDN do servidor para usar no myhostname
+PFHOSTNAME = socket.getfqdn()
+
+# Base do Configurador
+LXBASE="/usr/local/linuxplace"
+
+# Configuracoes a serem obtidas do Config
+PFMYNETWORK="192.168.0.0/24"
+LDAPHOST="127.0.0.1"
+LDAPBASEDN="dc=ceibe,dc=intra"
+SMTPRELAY="N"
+LDAPBINDPW="teste"
+PFRELAYHOST="smtp.bhz.terra.com.br"
+PFRELAYUSER="ceibe"
+PFRELAYPASS="a4g9h3e7"
+
+# Configuracoes de seguranca (Nao alterar caso nao saiba do que se trata)
+UNCOMMENTLINES="N"
+
+#Configuracoes Inalteraveis
+srcfile = LXBASE + "/stanza/main.cf.stanza"
+dstfile = "/tmp/main.cf.plucio"
+
+#
+LDAPBINDDN="cn=proxyuser,ou=Staff," + LDAPBASEDN
+
+# Le o arquivo stanza
+if os.path.exists(srcfile):
+	fp = open(srcfile,"r")
+else:
+	print "CANNOT OPEN STANZA FILE. ATUALIZE O REPOSITORIO!"
+
+stanza = fp.readlines()
+fp.close()
+
+# Abre o arquivo a ser gerado
+fp = open(dstfile,"w")
+
+#Faz as substituicoes necessarias
+for line in stanza:
+	newline=re.sub("%HOSTNAME%",PFHOSTNAME,line)
+	newline=re.sub("%MYNETWORK%",PFMYNETWORK,newline)
+	newline=re.sub("%LDAPHOST%",LDAPHOST,newline)
+	newline=re.sub("%LDAPBASEDN%",LDAPBASEDN,newline)
+	newline=re.sub("%LDAPBINDDN%",LDAPBINDDN,newline)
+	newline=re.sub("%LDAPBINDPW%",LDAPBINDPW,newline)
+	if ( SMTPRELAY == "Y" ):
+		if ( re.search("^###START-SMTPGATEWAYRELAY###$",newline)):
+			UNCOMMENTLINES="Y"
+		if ( re.search("^###END-SMTPGATEWAYRELAY###$",newline)):
+			UNCOMMENTLINES="N"
+		newline=re.sub("%RELAYHOST%",PFRELAYHOST,newline)
+		newline=re.sub("%RELAYUSER%",PFRELAYUSER,newline)
+		newline=re.sub("%RELAYPASS%",PFRELAYPASS,newline)
+	if ( UNCOMMENTLINES == "Y" ):
+		newline=re.sub("^#","",newline)
+	fp.write(newline)
+	
+
+# Fecha o arquivo gerado
+fp.close()
+sys.exit(0)
