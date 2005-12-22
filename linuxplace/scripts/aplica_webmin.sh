69806a27a90d1d68ae8ae4594eb7d968fde8338f
fams <fams@13f11263-bcd0-4e9e-95d0-45e2dbd4ab20>
2005-12-22 22:10:57 +0000

    Configura o webmin e o  idx

---
+++ linuxplace/scripts/aplica_webmin.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+LXHOME=/usr/local/linuxplace
+#Pega variáveis
+. $LXHOME/config/ldap
+. $LXHOME/config/lxn
+
+#variáveis auxiliares
+#CAMINHOS
+STANZAHOME=$LXHOME/stanza
+WEBMINHOME=$STANZAHOME/webmin
+ETCWEBMIN=/etc/webmin
+
+####Miniserv config
+FQDN=$(hostname -f)
+MYNETWORKA=$(echo $MYNETWORK|cut -f1 -d/)
+MYNETWORKM=$(ipcalc -n  $MYNETWORK|grep Netmask:|awk '{print $2}')
+sed -e "s/%FQDN%/$FQDN/;s/%LOCALNET%/$MYNETWORKA\/$MYNETWORKM/" $WEBMINHOME/miniserv.conf >$ETCWEBMIN/miniserv.conf
+
+#####IDXLDAPACCOUNTS
+mkdir -p $ETCWEBMIN/idxldapaccounts
+sed -e "s/%SUFFIX%/$SUFFIX/;s/%LDAPPROXYPASS%/$LDAPPROXYPASS/" $WEBMINHOME/idxldapaccounts/config >$ETCWEBMIN/idxldapaccounts/config
+
+###Usuario de gereciamento
+adduser --disabled-password --gecos "Usuario webmin" admproxy
+passwd -u admproxy
+echo admproxy:$WEBMINPW |/usr/sbin/chpasswd
+
+grep -v admproxy $ETCWEBMIN/miniserv.users >$ETCWEBMIN/miniserv.users.$$
+echo -e "admproxy:0:0::\n" >>$ETCWEBMIN/miniserv.users.$$
+mv $ETCWEBMIN/miniserv.users $ETCWEBMIN/miniserv.users.old
+mv $ETCWEBMIN/miniserv.users.$$ $ETCWEBMIN/miniserv.users
+
+grep -v admproxy $ETCWEBMIN/webmin.acl >$ETCWEBMIN/webmin.acl.$$
+echo "admproxy: sysstats squid idxldapaccounts postfix" >>$ETCWEBMIN/webmin.acl.$$
+mv $ETCWEBMIN/webmin.acl $ETCWEBMIN/webmin.acl.old
+mv $ETCWEBMIN/webmin.acl.$$ $ETCWEBMIN/webmin.acl
+#acl do admproxy
+cp -f $WEBMINHOME/squid/admproxy.acl $ETCWEBMIN/squid/
+cp -f $WEBMINHOME/sysstats/config $ETCWEBMIN/sysstats/
