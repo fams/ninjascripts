66043fef195328c02593215c5d496aec88a77430
fams <fams@13f11263-bcd0-4e9e-95d0-45e2dbd4ab20>
2006-05-16 12:24:02 +0000

    Modificado para suportar tanto velox como virtua

--- linuxplace/scripts/testacon.sh
+++ linuxplace/scripts/testacon.sh
@@ -1,18 +1,30 @@
 INETIFACE=eth1
 PEER=$(route -n|grep $INETIFACE|grep ^0.0.0.0|awk '{print $2}')
+OPERADORA=virtua
 function reconecta(){
-	/sbin/shorewall clear
-	ifdown $INETIFACE
-	sleep 10
-	ifup $INETIFACE
-	/sbin/shorewall start
+	case "$1" in
+	"virtua")
+		/sbin/shorewall clear
+		ifdown $INETIFACE
+		sleep 10
+		ifup $INETIFACE
+		/sbin/shorewall start
+	;;
+	"velox")
+		/sbin/shorewall clear
+		poff dsl-provider
+		sleep 10
+		pon dsl-provider
+		/sbin/shorewall start
+	;;
+esac
 }
 
 if [ -z "$PEER" ];then
-	reconecta
+	reconecta $OPERADORA
 fi
 result=$(ping -w 10 $PEER | grep  "100% packet loss")
 if [ ! -z "$result" ];then
-	reconecta
+	reconecta $OPERADORA
 fi
 
