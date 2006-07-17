8007bcddb35d99649e321a6fe306e36dec03c826
pedro <pedro@13f11263-bcd0-4e9e-95d0-45e2dbd4ab20>
2006-07-17 12:37:45 +0000

    Checkpoint Ldap

---
+++ linuxplace/scripts/teste-ldap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Script para teste/recovery da base LDAP
+#
+set -x
+
+umask 077
+BACKHOME="/var/backups"
+INITSCRIPT="/etc/init.d/ldap"
+TESTE=`ldapsearch -x -LLL|grep uid=root,ou=People,dc=omb,dc=com,dc=br|cut -d":" -f 2`
+if [ -z $TESTE ]; then
+	$INITSCRIPT restart
+	echo "reiniciei " `date +"%d-%m-%Y %H:%M:%s"` >> /var/log/testeldap
+	sleep 10
+	TESTE=`ldapsearch -x -LLL|grep uid=root,ou=People,dc=omb,dc=com,dc=br|cut -d":" -f 2`
+	if [ -z $TESTE ]; then
+		DATA_ATUAL=`date -I`
+		HORA_ATUAL=`date +%T`
+		tar czf $BACKHOME/base_backup_ldap_$DATA_ATUAL-$HORA_ATUAL$.tar.gz
+        	db_recover -v -h /var/lib/ldap >> /var/log/db_recover_log
+        	$INITSCRIPT start
+		echo "Recovery " `date +"%d-%m-%Y %H:%M:%S"` >> /var/log/testeldap
+	fi
+fi
+ps -ef|grep db_chec|grep -v grep
+ret=$?
+if [ $ret -ne 0 ];then
+	db_checkpoint -h /var/lib/ldap/ -p 5 -L /var/log/check_point.log
+fi
