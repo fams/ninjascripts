293c20c4ab50c5b2ba58140f2478c8809c1a11cd
plucio <plucio@13f11263-bcd0-4e9e-95d0-45e2dbd4ab20>
2007-03-12 13:28:54 +0000

    Versao inicial README com instrucoes quanto ao uso do vfailover

---
+++ linuxplace/scripts/vfailover/rota.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Comandos
+IP="/sbin/ip"
+
+#DEBUG
+#echo $0 $1 $2 $3 >> /tmp/debug
+
+# Variaveis
+VELOXIF="ppp0"
+EMBRAIF="eth1"
+EMBRAGW="200.251.121.1"
+
+action=$1
+
+function showdefroute() {
+  if [ "$($IP route list |grep ^default |grep $EMBRAIF)" ]; then
+	echo dedicado
+  elif [ "$($IP route list |grep ^default |grep $VELOXIF)" ]; then
+	echo velox
+  else
+  	echo panic
+  fi
+}
+
+function setveloxdef() {
+  $IP route del default 2>/dev/null
+  $IP route add default dev $VELOXIF
+  $IP route flush cache
+  showdefroute
+}
+
+function setembradef() {
+  $IP route del default 2>/dev/null
+  $IP route add default via $EMBRAGW dev $EMBRAIF
+  $IP route flush cache
+  showdefroute
+}
+
+
+case "$action" in
+  show)
+    showdefroute
+  ;;
+  setvelox)
+    setveloxdef
+  ;;
+  setembratel)
+    setembradef
+  ;;
+  setdedicado)
+    setembradef
+  ;;
+  *)
+    echo Erro_de_uso
+  ;;
+esac
