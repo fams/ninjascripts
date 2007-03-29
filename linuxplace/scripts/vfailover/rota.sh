37990086361d939473312ac9a17fad1d40a05600
fams <fams@13f11263-bcd0-4e9e-95d0-45e2dbd4ab20>
2007-03-29 01:36:53 +0000

    Generalizacao do script agora com arquivos de configuracao start stop faltando buscar no unitas adicionado mudanca de no-ip o script ainda Ã© muito manual precisa de muita configuracao

--- linuxplace/scripts/vfailover/rota.sh
+++ linuxplace/scripts/vfailover/rota.sh
@@ -7,35 +7,41 @@ IP="/sbin/ip"
 
 #DEBUG
 #echo $0 $1 $2 $3 >> /tmp/debug
-
-# Variaveis
-VELOXIF="ppp0"
-EMBRAIF="eth1"
-EMBRAGW="200.251.121.1"
+#config file
+. /etc/vfailover.conf
 
 action=$1
 
 function showdefroute() {
-  if [ "$($IP route list |grep ^default |grep $EMBRAIF)" ]; then
-	echo dedicado
-  elif [ "$($IP route list |grep ^default |grep $VELOXIF)" ]; then
-	echo velox
+data=`date +%H:%M:%S`
+echo $data show >>/tmp/show
+  if [ "$($IP route list |grep ^default |grep $PRI_DEV)" ]; then
+	echo primary
+  elif [ "$($IP route list |grep ^default |grep $SEC_DEV)" ]; then
+	echo secondary
   else
   	echo panic
   fi
+echo $data show >>/tmp/show
 }
 
-function setveloxdef() {
+function setsecondarydef() {
   $IP route del default 2>/dev/null
-  $IP route add default dev $VELOXIF
+  $IP route add default dev $SEC_DEV
   $IP route flush cache
+  /usr/bin/killall no-ip 2>/dev/null
+  sleep 1
+  /usr/bin/no-ip -c /etc/no-ip.conf.velox 2>/dev/null
   showdefroute
 }
 
-function setembradef() {
+function setprimarydef() {
   $IP route del default 2>/dev/null
-  $IP route add default via $EMBRAGW dev $EMBRAIF
+  $IP route add default via $PRI_GW dev $PRI_DEV
   $IP route flush cache
+  /usr/bin/killall no-ip >/dev/null
+  sleep 1
+ /usr/bin/no-ip -c /etc/no-ip.conf.virtua 2>/dev/null
   showdefroute
 }
 
@@ -44,16 +50,14 @@ case "$action" in
   show)
     showdefroute
   ;;
-  setvelox)
-    setveloxdef
+  setsecondary)
+    setsecondarydef
   ;;
-  setembratel)
-    setembradef
-  ;;
-  setdedicado)
-    setembradef
+  setprimary)
+    setprimarydef
   ;;
   *)
     echo Erro_de_uso
   ;;
 esac
+echo SAINDO >>/tmp/tulio
