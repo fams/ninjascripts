4e8539bc66d2503e9b736d39f056ff9b891e288e
pedro <pedro@13f11263-bcd0-4e9e-95d0-45e2dbd4ab20>
2007-07-19 18:07:49 +0000

    versao modificada pelo plucio para funcionar com velox quando a rota padrao joga direto para a ppp

--- linuxplace/scripts/testacon.sh
+++ linuxplace/scripts/testacon.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
 # Reconex√o velox/virtua
 # necessario colocar no crond
 # author: <fams@linuxplace.com.br>
 # Version: $Id$
+PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-INETIFACE=eth1
+INETIFACE=ppp0
 PEER=$(/sbin/route -n|grep $INETIFACE|grep ^0.0.0.0|awk '{print $2}')
-OPERADORA=virtua
+OPERADORA=velox
 function reconecta(){
 	case "$1" in
 	"virtua")
@@ -19,8 +21,11 @@ function reconecta(){
 		/sbin/shorewall clear
 		poff dsl-provider
 		sleep 10
+		/sbin/ifconfig eth1 up
 		pon dsl-provider
 		/sbin/shorewall start
+		/etc/init.d/no-ip restart
+		/etc/init.d/bind9 restart
 	;;
 esac
 }
@@ -28,8 +33,12 @@ esac
 if [ -z "$PEER" ];then
 	reconecta $OPERADORA
 fi
+if ( [ "$PEER" = "0.0.0.0" ] && [ "$OPERADORA" = "velox" ] ); then
+  PEER=$(/sbin/ip addr list |grep $INETIFACE |grep peer|awk '{print $4}'|cut -d '/' -f 1)
+fi
 result=$(/bin/ping -w 10 $PEER | grep  "100% packet loss")
 if [ ! -z "$result" ];then
 	reconecta $OPERADORA
 fi
+#echo $PEER
 
