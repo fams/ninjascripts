23f5bb7508d051231143f44d60eecc846ecb62eb
fams <fams@13f11263-bcd0-4e9e-95d0-45e2dbd4ab20>
2008-11-26 16:54:36 +0000

    base para o novo getlkeys

---
+++ pacotes/getkeys/src/script/getkeys2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+URL="https://192.168.0.2/update/extended2.php" 
+XMLFILE=$(/bin/mktemp) 
+#Header 
+function XMLheader(){
+    echo '<?xml version="1.0" encoding="UTF-8"?>
+<gk version="1.1">
+'
+}
+function XMLfooter(){
+    echo "</gk>"
+}
+function XMLlast(){
+    echo -e "\t<last>"
+    while read LAST;do
+        LASTUSER=$(echo $LAST|cut -f 1 -d" ") 
+        LASTLOGON=$(echo $LAST|cut -f 6 -d" ") 
+        LASTLOGOFF=$(echo $LAST|cut -f 8 -d" ") 
+        echo -e "\t\t<loginentry user=\"$LASTUSER\" logon=\"$LASTLOGON\"
+logoff=\"$LASTLOGOFF\" />"
+    done < <(/usr/bin/last -R|grep -v "^$"|grep -v ^wtmp)
+    echo -e "\t</last>"
+}
+function XMLuptime(){
+    echo -e "\t<uptime>"
+    /usr/bin/uptime
+    echo -e "\t</uptime>"
+}
+############Print XML##########
+XMLheader >>$XMLFILE
+XMLlast   >>$XMLFILE
+XMLuptime >>$XMLFILE
+XMLfooter >>$XMLFILE
+cat $XMLFILE 
+STR1=$(perl -pe 's/(\W)/"%".unpack"H2",$1/ge' $XMLFILE) 
+curl -k -X POST -F teste=$STR1 $URL 
+rm $XMLFILE 
