5feb4536036750e10d343cb1519eaa901f7ba3eb
fams <fams@13f11263-bcd0-4e9e-95d0-45e2dbd4ab20>
2009-03-18 12:40:20 +0000

    Script de instalaçao para distros não debian

---
+++ pacotes/getkeys/src/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+getent passwd svccap >/dev/null
+ret=$?
+if [ $ret -ne 0 ];then
+	echo "Nao existe usuario svccap"
+fi
+if [ ! -d /usr/local/share/linuxplace ]; then
+	mkdir -p /usr/local/share/linuxplace
+fi
+if [ ! -d /usr/local/etc ]; then
+	mkdir -p /usr/local/etc
+fi
+if [ ! -d /usr/local/share/doc/getkeys ]; then
+	mkdir -p /usr/local/share/doc/getkeys
+fi
+if [ ! -d ~svccap/.ssh ];then
+	group=$(getent passwd svccap|cut -f 4 -d: )
+	mkdir -p ~svccap/.ssh
+	chown svccap:$group ~svccap/.ssh
+fi
+
+cp -pa doc/getkeys.conf.default /usr/local/etc
+cp -pa share/* /usr/local/share/linuxplace
+cp -pa doc/* /usr/local/share/doc/getkeys/
+
+cp script/getkeys.sh /usr/local/sbin
+cp script/extended.sh /usr/local/sbin
+chmod +x /usr/local/sbin/{getkeys.sh,extended.sh}
+
+echo "Lembre-se de configurar o cron.d, o exemplo está em /usr/local/share/doc/getkeys"
