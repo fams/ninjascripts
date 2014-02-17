4aab236d15074bf9de30e14b32089349a3369379
Fernando Augusto <fams@linuxplace.com.br>
2014-02-17 22:23:11 +0000

    Removido Lixo

--- linuxplace/scripts/ninjainstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#Instalaçao ninja
-#author: Fernando Augusto Medeiros Silva <fams@linuxplace.com.br>
-#
-#detectando ambiente
-DIALOG=/usr/bin/gdialog
-
-$DIALOG --msgbox 'Esse é um instalador destrutivo, lembre-se disso!!!' 10 40
